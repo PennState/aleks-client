2beff2d550a4987d34432d9dc8580364c429d069
Steve Moyer <smoyer@psu.edu>
2019-11-14 10:29:55 -0500

    Adds error checking and godocs

--- pkg/aleks/client.go
+++ pkg/aleks/client.go
@@ -1,18 +1,25 @@
 package aleks
 
 import (
+	"errors"
 	"net/http"
-	"net/url"
+	stdurl "net/url"
 
 	"github.com/kelseyhightower/envconfig"
-	log "github.com/sirupsen/logrus"
 )
 
 const (
-	aleksEnvconfigPrefix = "aleks"
-	aleksURL             = "https://secure.aleks.com/xmlrpc"
+	// AleksEnvconfigPrefix indicates that all environment variables used
+	// by this library will start with ALEKS_.
+	AleksEnvconfigPrefix = "aleks"
+	// AleksDefaultURL is used to access the Aleks service if an alternate
+	// URL is not provided via either the parameterized NewClient
+	// constructor or the no-parameters NewClientFromEnv constructor.
+	AleksDefaultURL = "https://secure.aleks.com/xmlrpc"
 )
 
+// Client contains the basic XML-RPC parameters required to make a call
+// to the Aleks service.
 type Client struct {
 	url      string
 	username string
@@ -20,8 +27,10 @@ type Client struct {
 	trans    http.RoundTripper
 }
 
-func NewClient(URL, username, password string) (*Client, error) {
-	return newClient(URL, username, password, &RoundTripper{})
+// NewClient returns a new Aleks client given an optional URL, a username
+// and a password.
+func NewClient(url, username, password string) (*Client, error) {
+	return newClient(url, username, password, &RoundTripper{})
 }
 
 type clientEnvConfig struct {
@@ -30,26 +39,37 @@ type clientEnvConfig struct {
 	Password string `required:"true"`
 }
 
+// NewClientFromEnv returns a new Aleks client from environment variables
+// as follows:
+//
+// - ALEKS_URL      (Optional - see default in constants)
+// - ALEKS_USERNAME (Required)
+// - ALEKS_PASSWORD (Required)
+//
+// It is important to note that the individual Aleks XMLRPC calls will
+// generally required additional parameters.
 func NewClientFromEnv() (*Client, error) {
 	cfg := clientEnvConfig{}
-	err := envconfig.Process(aleksEnvconfigPrefix, &cfg)
+	err := envconfig.Process(AleksEnvconfigPrefix, &cfg)
 	if err != nil {
 		return nil, err
 	}
-	log.Info("Client config: ", cfg)
 	return newClient(cfg.URL, cfg.Username, cfg.Password, &RoundTripper{})
 }
 
-func newClient(URL, username, password string, trans http.RoundTripper) (*Client, error) {
-	if URL == "" {
-		URL = aleksURL
+func newClient(url, username, password string, trans http.RoundTripper) (*Client, error) {
+	if url == "" {
+		url = AleksDefaultURL
 	}
-	_, err := url.Parse(URL)
+	_, err := stdurl.Parse(url)
 	if err != nil {
 		return nil, err
 	}
+	if username == "" || password == "" {
+		return nil, errors.New("username and password parameters are both required")
+	}
 	return &Client{
-		url:      URL,
+		url:      url,
 		username: username,
 		password: password,
 		trans:    trans,
