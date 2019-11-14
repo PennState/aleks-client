75f3699f2ce137fca9067326ce2534bc796ebac6
Steve Moyer <smoyer@psu.edu>
2019-11-14 13:43:48 -0500

    Adds package documentation

---
+++ pkg/aleks/pkg.go
@@ -0,0 +1,24 @@
+/*
+Copyright 2019 The Pennsylvania State University
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/*
+Package aleks provides a client library to access data provided by
+the McGraw-Hill Aleks service.  Currently, two methods to retrieve Aleks
+placement report records are provided by an instantiated client.  See
+the sample program in the cmd/placementreport package for an example of
+how this library can be used.
+*/
+package aleks
